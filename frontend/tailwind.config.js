e37e3ea8779fe8bf1f03c6757bb7690deae2a8e3
vivek <vivekvasant310@gmail.com>
2024-12-31 14:57:55 +0530

    tailwind config change

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from 'tailwind-scrollbar';
 export default {
   content: [
     "./index.html",
@@ -8,7 +9,7 @@ export default {
     extend: {},
   },
   plugins: [
-    require('tailwind-scrollbar'),
+    tailwindScrollbar,
   ],
 }
 
